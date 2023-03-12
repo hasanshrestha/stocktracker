bfb060812256901b9a468035d6e0178fe7d725a6
Hasan Shrestha <hasan.xtha.5311@gmail.com>
2023-03-12 13:37:32 +0545

    production changes 2

--- stocktracker/settings.py
+++ stocktracker/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static") #for production
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # to upload in folder
